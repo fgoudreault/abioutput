cee28b7aeb6d9f4fc1b8a4ca2647dcd9a61dbaf4
fgoudreault <felix.goudreault82@gmail.com>
2017-09-15 16:16:48 -0400

    Added minimalist DOS parser

--- abioutput/__init__.py
+++ abioutput/__init__.py
@@ -1,2 +1,3 @@
 from .output_parser import OutputParser
+from .dos_parser import DOSParser
 from .constants import *
---
+++ abioutput/dos_parser.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class DOSParser:
+    def __init__(self, path):
+        self.data, self.titles = self._get_data_from_file(path)
+
+    def _get_data_from_file(self, path):
+        # this is a numpy array with the data.
+        data = np.loadtxt(path)
+        # we now need the columns title
+        titles = self._get_titles(path)
+        return data, titles
+
+    def _get_titles(self, path):
+        with open(path) as f:
+            lines = f.readlines()
+
+        for i, line in enumerate(lines):
+            if not line.startswith("#"):
+                titlesline = lines[i - 2]
+                # titlesline2 = lines[i - 1]
+                break
+        else:
+            raise LookupError("There is only comments in this file!")
+        # titlesline looks like this:
+        #  #        energy        DOS       Integr. DOS      DOS         DOS
+        # i'm not quite sure how could I efficiently get all titles since
+        # this line could change depending of the input options.
+        # for now, just return it knowing the first column is the energy
+        # and the second the total DOS.
+        return titlesline
