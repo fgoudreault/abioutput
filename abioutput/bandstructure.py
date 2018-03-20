85143799c8e0935132afe49c14dae2799b557ce3
fgoudreault <felix.goudreault82@gmail.com>
2018-03-20 12:35:06 -0400

    Added more functionalities to bandstructure and more docstrings

--- abioutput/bandstructure.py
+++ abioutput/bandstructure.py
@@ -14,16 +14,37 @@ HIGH_SYM_PTS = {"FCC": {"L": (0.5, 0.0, 0.0),
 
 
 class Bandstructure:
-    def __init__(self, kpts, eigenvalues, fermi_band=None):
+    """Object that handles band structures given a list of kpts and
+    eigenvalues. Can also read directly the eigenvalues from a file using
+    the from_file classmethod. In this case, kpts and eigenvalues args
+    are directly given to the init method.
+
+    Parameters
+    ----------
+    kpts : list of kpts
+    eigenvalues : list of eigenvalues for each kpt.
+    fermi_band : int, optional
+                 If not None, gives the band index of the last valence band.
+                 Thus, it can compute the fermi energy from the maximum value
+                 of that band.
+    fermi_energy : float, optional
+                   This parameter lets define manually the fermi energy
+                   (must be same units as the eigenvalues).
+    """
+    def __init__(self, kpts, eigenvalues, fermi_band=None, fermi_energy=None):
         self.kpts = kpts
         self.eigenvalues = eigenvalues
         self.bands = self._get_bands(eigenvalues)
         self.fermi_band = fermi_band
-        self.fermi_energy = 0
+        self.fermi_energy = fermi_energy
         if self.fermi_band is not None:
+            if self.fermi_energy is None:
+                raise ValueError("fermi_energy is already defined.")
             self.fermi_energy = max(self.bands[self.fermi_band])
 
-    def plot(self, symmetry="none", color="k", save_at=None, show=True):
+    def plot(self, bands=None, symmetry="none",
+             color="k", save_at=None, show=True,
+             other_k_labels=None):
         """Plot the bandstructure.
 
         Parameters
@@ -36,6 +57,9 @@ class Bandstructure:
                Gives the line color of the bandstructure. If set to None,
                the color won't be set and it will be a standard multi curve
                pyplot.
+        bands: list, optional
+               Selects the range of bands to plot (starting from 0).
+               If set to None, all bands are shown.
         save_at: str, optional
                  If not None, gives the path to where the figure will be saved.
         show: bool, optional
@@ -43,11 +67,17 @@ class Bandstructure:
         """
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        for band in self.bands:
+        considered_bands = self.bands
+        fermi_energy = self.fermi_energy
+        if self.fermi_energy is None:
+            fermi_energy = 0
+        if bands is not None:
+            considered_bands = self.bands[range(bands[0], bands[1] + 1), :]
+        for band in considered_bands:
             if color is not None:
-                ax.plot(band - self.fermi_energy, color=color)
+                ax.plot(band - fermi_energy, color=color)
             else:
-                ax.plot(band)
+                ax.plot(band - fermi_energy)
         labels, labels_loc = self._get_sym_pts_labels(symmetry)
         ax.set_xticks(labels_loc, labels)
         ax.set_ylabel("Energy")
@@ -84,9 +114,17 @@ class Bandstructure:
         return bands
 
     @classmethod
-    def from_file(cls, path, fermi_band=None):
+    def from_file(cls, path, conversion_factor=None, **kwargs):
+        """Classmethod to read kpts and eigenvalues directly from an EIG file.
+
+        Parameters
+        ----------
+        path : The EIG file path.
+        conversion_factor : optional, float
+                            Multiplies all eigenvalues with this factor.
+        """
         eigparser = EIGParser.from_file(path)
-        kptslist = eigparser.data
-        kpts = [x["coordinates"] for x in kptslist]
-        eigenvalues = [x["eigenvalues"] for x in kptslist]
-        return cls(kpts, eigenvalues, fermi_band=fermi_band)
+        eigs = np.array(eigparser.data["eigenvalues"])
+        if conversion_factor is not None:
+            eigs *= conversion_factor
+        return cls(eigparser.data["coordinates"], eigs, **kwargs)
