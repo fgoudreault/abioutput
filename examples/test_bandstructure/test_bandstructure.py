b296b1a00b779882787835fe23f4f1995b561e2a
Félix Antoine Goudreault <felix.goudreault82@gmail.com>
2018-06-07 10:58:10 -0400

    Need to remove examples from pytest

--- examples/test_bandstructure/test_bandstructure.py
+++ examples/test_bandstructure/test_bandstructure.py
@@ -2,14 +2,14 @@ from abioutput import Bandstructure
 
 
 path = "GaAs_no_SO_EIG"
-fermi_band=3
+fermi_band = 3
 b = Bandstructure.from_file(path,
                             fermi_band=fermi_band)
 plot1 = b.plot(ylabel="E (eV)")
 plot1.set_curve_label("No SO", 0)
 
 path = "GaAs_SO_EIG"
-fermi_band=7
+fermi_band = 7
 b2 = Bandstructure.from_file(path,
                              fermi_band=fermi_band)
 plot2 = b2.plot(ylabel="E (eV)", color="b")
